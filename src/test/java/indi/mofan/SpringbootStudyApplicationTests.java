6373e282966323fb353b30b6bd61658d38fc4ef9
mofan <cy.mofan@foxmail.com>
2022-10-11 17:45:11 +0800

    init project

---
+++ src/test/java/indi/mofan/SpringbootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package indi.mofan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
