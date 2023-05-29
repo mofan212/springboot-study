bd9a02e8500e5261fde6c7046d2ba072eef9851b
mofan <cy.mofan@foxmail.com>
2023-05-29 16:04:48 +0800

    拓展点六: ApplicationContextAwareProcessor

---
+++ extension-point/src/main/java/indi/mofan/summary/a06/A06ExtensionPoint.java
@@ -0,0 +1,58 @@
+package indi.mofan.summary.a06;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author mofan
+ * @date 2023/5/29 14:05
+ */
+@SpringBootApplication
+public class A06ExtensionPoint {
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(A06ExtensionPoint.class, args);
+        context.close();
+    }
+
+
+    /**
+     * <p>
+     * 类似的接口还有：
+     * <ul>
+     *     <li>EnvironmentAware</li>
+     *     <li>EmbeddedValueResolverAware</li>
+     *     <li>ResourceLoaderAware</li>
+     *     <li>ApplicationEventPublisherAware</li>
+     *     <li>MessageSourceAware</li>
+     *     <li>ApplicationStartupAware</li>
+     * </ul>
+     * </p>
+     */
+    @Component
+    static class MyApplicationContextAware implements ApplicationContextAware {
+        private ApplicationContext applicationContext;
+
+        @Override
+        public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+            this.applicationContext = applicationContext;
+            System.out.println("2. Spring 上下文 ApplicationContext 被注入");
+        }
+    }
+
+    @Component
+    static class MyApplicationContext {
+        private ApplicationContext applicationContext;
+
+        @Autowired
+        public void setApplicationContext(ApplicationContext applicationContext) {
+            this.applicationContext = applicationContext;
+            System.out.println("1. 使用 @Autowired 注入 ApplicationContext");
+        }
+    }
+}
