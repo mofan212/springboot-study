d2b7e5ab1461ec57a2253bf8ac088436eb95ef1c
mofan <cy.mofan@foxmail.com>
2023-10-02 22:43:42 +0800

    拓展点: SmartInitializingSingleton

--- extension-point/src/main/java/indi/mofan/summary/a10/A10ExtensionPoint.java
+++ extension-point/src/main/java/indi/mofan/summary/a10/A10ExtensionPoint.java
@@ -1,10 +1,8 @@
 package indi.mofan.summary.a10;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.SmartInitializingSingleton;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,57 +17,30 @@ import org.springframework.stereotype.Component;
 public class A10ExtensionPoint {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(A10ExtensionPoint.class, args);
-        // context.close(); 不直接关闭容器，而是通过 System.exit() 关闭，此时会触发 shutdown hooks
-        System.out.printf("[%s]: main 方法执行完毕，准备关闭容器\n", Thread.currentThread().getName());
-        System.exit(0);
-    }
-
-    @Component
-    static class A09Dependence {
+        context.close();
     }
 
     private static final String BEAN_NAME = "myBean";
 
     @Component(BEAN_NAME)
-    static class MySmartInitializingSingleton implements InitializingBean, SmartInitializingSingleton, DisposableBean {
-        private A09Dependence dependence;
-
-        @Autowired
-        public void setDependence(A09Dependence dependence) {
-            this.dependence = dependence;
-            System.out.println("1. MySmartInitializingSingleton 注入 dependence");
-        }
-
+    static class MySmartInitializingSingleton implements InitializingBean, SmartInitializingSingleton {
         @Override
-        public void afterPropertiesSet() throws Exception {
-            System.out.println("3. 执行了 afterPropertiesSet 方法");
+        public void afterPropertiesSet() {
+            System.out.println("1. 执行了 afterPropertiesSet 方法");
         }
 
         @Override
         public void afterSingletonsInstantiated() {
-            System.out.println("5. 执行了 afterSingletonsInstantiated 方法");
-        }
-
-        @Override
-        public void destroy() throws Exception {
-            System.out.printf("[%s]: 6. 执行了 destroy 方法\n", Thread.currentThread().getName());
+            System.out.println("3. 执行了 afterSingletonsInstantiated 方法");
         }
     }
 
     @Component
     static class MyBeanPostProcessor implements BeanPostProcessor {
         @Override
-        public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-            if (BEAN_NAME.equals(beanName)) {
-                System.out.println("2. 执行了 postProcessBeforeInitialization 方法");
-            }
-            return bean;
-        }
-
-        @Override
         public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
             if (BEAN_NAME.equals(beanName)) {
-                System.out.println("4. 执行了 postProcessAfterInitialization 方法");
+                System.out.println("2. 执行了 postProcessAfterInitialization 方法");
             }
             return bean;
         }
