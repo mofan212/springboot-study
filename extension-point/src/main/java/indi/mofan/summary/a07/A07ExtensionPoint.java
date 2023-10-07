472a3ac87a0a4255bd523d0e7525f7467c8ad13c
mofan <cy.mofan@foxmail.com>
2023-10-07 20:28:52 +0800

    移动代码位置

--- extension-point/src/main/java/indi/mofan/summary/a07/A07ExtensionPoint.java
+++ extension-point/src/main/java/indi/mofan/summary/a07/A07ExtensionPoint.java
@@ -2,12 +2,15 @@ package indi.mofan.summary.a07;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
+import org.springframework.util.Assert;
 
 /**
  * @author mofan
@@ -53,6 +56,34 @@ public class A07ExtensionPoint {
         public void setApplicationContext(ApplicationContext applicationContext) {
             this.applicationContext = applicationContext;
             System.out.println("1. 使用 @Autowired 注入 ApplicationContext");
+        }
+    }
+
+    @Component
+    static class MyFirstBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+        @Autowired
+        private ApplicationContext context;
+
+        @Override
+        public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+            Assert.isNull(context, "依赖注入的 ApplicationContext 不是 null");
+        }
+    }
+
+    @Component
+    static class MySecondBeanFactoryPostProcessor implements BeanFactoryPostProcessor, ApplicationContextAware {
+
+        private ApplicationContext context;
+
+        @Override
+        public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+            Assert.notNull(context, "使用 ApplicationContextAware 注入的 ApplicationContext 为 null");
+        }
+
+        @Override
+        public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+            this.context = applicationContext;
         }
     }
 }
