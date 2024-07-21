6d0ae41633fd702e10f99063bd9a87131892122b
mofan <cy.mofan@foxmail.com>
2024-07-21 20:19:03 +0800

    事件链

--- extension-point-summary/src/main/java/indi/mofan/event/SimpleSpringEventListenerTest.java
+++ extension-point-summary/src/main/java/indi/mofan/event/SimpleSpringEventListenerTest.java
@@ -23,11 +23,20 @@ public class SimpleSpringEventListenerTest {
     @Component
     private static class UserRegisterEventListener {
         @EventListener
-        public void doEvent(UserRegisterEvent event) {
+        public EventChain doEvent(UserRegisterEvent event) {
             log.info("监听到用户注册事件: {}", event.info());
+            return new EventChain("事件链");
+        }
+
+        @EventListener
+        public void doChain(EventChain chain) {
+            log.info("监听到事件链: {}", chain.info());
         }
     }
 
     private record UserRegisterEvent(String info) {
+    }
+
+    private record EventChain(String info) {
     }
 }
