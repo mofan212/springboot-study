de6b0b413909333c6a73f48f7094cd5aa9b422a8
mofan <cy.mofan@foxmail.com>
2025-04-13 20:22:34 +0800

    剔除无用代码

--- conditional/missing-bean/service/src/main/java/indi/mofan/service/impl/DefaultServiceImpl.java
+++ conditional/missing-bean/service/src/main/java/indi/mofan/service/impl/DefaultServiceImpl.java
@@ -7,16 +7,12 @@ import indi.mofan.service.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 /**
  * @author mofan
  * @date 2025/3/26 16:43
  */
 @Component(BeanNameConstant.DEFAULT_BEAN)
 public class DefaultServiceImpl implements MyService {
-
-    public static AtomicInteger integer = new AtomicInteger(0);
 
     private InjectComponent injectComponent;
 
@@ -25,17 +21,8 @@ public class DefaultServiceImpl implements MyService {
         this.injectComponent = injectComponent;
     }
 
-    public DefaultServiceImpl() {
-        integer.incrementAndGet();
-    }
-
     @Override
     public String getStr() {
         return "default " + injectComponent.getStr();
-    }
-
-    @Override
-    public Integer getInteger() {
-        return integer.get();
     }
 }
